99d7144b05670fc71790f2566147285d684a1ae1
Greg Xu <gouwa5700@gmail.com>
2022-05-25 01:51:52 +0800

    set up dependents and dependencies relationships

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,14 @@
 class Todo < ApplicationRecord
   belongs_to :project
+
+  has_many :todo_dependents, class_name: 'TodoChild',
+                               foreign_key: 'todo_id',
+                               dependent: :destroy
+
+  has_many :todo_dependencies, class_name: 'TodoChild',
+                             foreign_key: 'child_id',
+                             dependent: :destroy
+
+  has_many :dependents, through: :todo_dependents, source: :child
+  has_many :dependencies, through: :todo_dependencies, source: :todo
 end
--- app/models/todo_child.rb
+++ app/models/todo_child.rb
@@ -1,4 +1,6 @@
 class TodoChild < ApplicationRecord
   belongs_to :todo
   belongs_to :child, class_name: 'Todo'
+  validates :todo_id, presence: true
+  validates :child_id, presence: true
 end
