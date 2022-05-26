1887feb355a7a5e486807618053fe2e1010f139c
Greg Xu <gouwa5700@gmail.com>
2022-05-26 21:51:59 +0800

    search by project_id and name

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,7 @@ class TodosController < ApplicationController
 
   # GET /todos
   def index
-    @todos = params[:project_id].present? ? Project.find(params[:project_id]).todos : Todo.all
+    @todos = Todo.search(params)
   end
 
   # GET /todos/1
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,15 +2,34 @@ class Todo < ApplicationRecord
   belongs_to :project
 
   has_many :todo_dependents, class_name: 'TodoChild',
-                               foreign_key: 'todo_id',
-                               dependent: :destroy
+                             foreign_key: 'todo_id',
+                             dependent: :destroy
 
   has_many :todo_dependencies, class_name: 'TodoChild',
-                             foreign_key: 'child_id',
-                             dependent: :destroy
+                               foreign_key: 'child_id',
+                               dependent: :destroy
 
   has_many :dependents, through: :todo_dependents, source: :child
   has_many :dependencies, through: :todo_dependencies, source: :todo
 
   accepts_nested_attributes_for :todo_dependents, :todo_dependencies
+
+  scope :of_project, ->(project_id) { where('project_id = ?', project_id) }
+  scope :name_contains, ->(name) { where('name LIKE ?', '%' + Todo.sanitize_sql_like(name) + '%') }
+
+  def self.search(query)
+    scopes = []
+    scopes.push([:of_project, query[:project_id]]) if query.try(:[], :project_id)
+    scopes.push([:name_contains, query[:name]]) if query.try(:[], :name)
+
+    if scopes.empty?
+      all
+    else
+      send_chain(scopes)
+    end
+  end
+
+  def self.send_chain(scopes)
+    Array(scopes).inject(self) { |o, a| o.send(*a) }
+  end
 end
