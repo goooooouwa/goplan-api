1bbd50d7be427cee776cc0b789d60e7634c873bd
Greg Xu <gouwa5700@gmail.com>
2022-06-01 18:35:43 +0800

    update root todo with callbacks to update deps

--- app/jobs/todos_postpone_delayed_job.rb
+++ app/jobs/todos_postpone_delayed_job.rb
@@ -2,12 +2,8 @@ class TodosPostponeDelayedJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Todo.where.missing(:todo_dependencies).due_date_before(Time.current).in_batches.each do |root_todo|
-      delta = Time.current - root_todo.end_date
-      root_todo.update(end_date: root_todo.end_date + delta)
-      root_todo.dependents.each do |dependent|
-        dependent.update(start_date: dependent.start_date + delta, end_date: dependent.end_date + delta)
-      end
+    Todo.where.missing(:todo_dependencies).due_date_before(Time.current).in_batches.each_record do |root_todo|
+      root_todo.update(end_date: Time.current)
     end
   end
 end
