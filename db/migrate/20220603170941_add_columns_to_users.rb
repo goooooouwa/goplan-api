7c2a06625e97ecf711adc209624acac44c5f2c6f
Greg Xu <gouwa5700@gmail.com>
2022-06-04 01:09:58 +0800

    add user name and image_url to User

---
+++ db/migrate/20220603170941_add_columns_to_users.rb
@@ -0,0 +1,6 @@
+class AddColumnsToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :image_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_03_133320) do
+ActiveRecord::Schema.define(version: 2022_06_03_170941) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
-    t.datetime "target_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "target_date", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "todo_children", force: :cascade do |t|
     t.bigint "todo_id", null: false
     t.bigint "child_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["child_id"], name: "index_todo_children_on_child_id"
     t.index ["todo_id"], name: "index_todo_children_on_todo_id"
   end
@@ -35,14 +36,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_133320) do
     t.string "name"
     t.text "description"
     t.float "time_span"
-    t.datetime "start_date"
-    t.datetime "end_date"
+    t.datetime "start_date", precision: 6
+    t.datetime "end_date", precision: 6
     t.boolean "repeat"
     t.string "repeat_period"
     t.integer "repeat_times"
     t.integer "instance_time_span"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.boolean "status", default: false
     t.index ["project_id"], name: "index_todos_on_project_id"
   end
@@ -51,12 +52,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_133320) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "reset_password_sent_at", precision: 6
+    t.datetime "remember_created_at", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "provider"
     t.string "uid"
+    t.string "name"
+    t.string "image_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
