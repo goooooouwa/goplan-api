bb4b81fcf76caa040f65a1ec4234aa5be491ffb7
Greg Xu <gouwa5700@gmail.com>
2022-06-04 01:31:10 +0800

    convert rails_api to rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
-class ApplicationController < ActionController::API
-  include ActionController::RequestForgeryProtection
-  include ActionView::Layouts
-  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 end
