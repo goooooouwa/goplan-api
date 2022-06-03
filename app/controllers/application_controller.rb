abba32dacfbe23aea15ff53a192ea7b185c1573f
Greg Xu <gouwa5700@gmail.com>
2022-06-04 00:56:04 +0800

    setup CSRF protection for Rails API with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   include ActionController::RequestForgeryProtection
+  include ActionView::Layouts
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
   before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "devise/sessions#new"
 end
