7fa08819bb3bf8ebc44ea0335a043c4cb2b205d7
Greg Xu <gouwa5700@gmail.com>
2022-06-03 22:36:06 +0800

    fix protect_from_forgery issue
    Ref: https://stackoverflow.com/questions/42795288/rails-how-to-implement-protect-from-forgery-in-rails-api-mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::API
+  include ActionController::RequestForgeryProtection
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
   before_action :authenticate_user!
 end
