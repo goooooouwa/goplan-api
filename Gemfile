94571231a9e74acc4ffc4b85efec0bdf09e94342
Greg Xu <gouwa5700@gmail.com>
2022-06-03 16:23:50 +0800

    generate devise user

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem "puma", "~> 4.3"
 gem "jbuilder"
 
 gem "devise"
+gem "omniauth"
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :todos do
     member do
       put :dependencies, to: 'todos#update_dependencies'
