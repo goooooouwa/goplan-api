a152651f47760ce8bdc5901a5ba29f19337fcc4b
Greg Xu <gouwa5700@gmail.com>
2022-06-04 02:07:37 +0800

    rails generate doorkeeper:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  use_doorkeeper
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     delete 'sign_out', :to => 'devise/sessions#destroy'
