2883d2e1cd4f91fb8089c8825d17c5af57896eea
Greg Xu <gouwa5700@gmail.com>
2022-06-03 16:13:55 +0800

    config devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'https://goplan-api.herokuapp.com/', port: 5000 }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
