5265f8d94bf049277967b188ed878e56ba6f3049
Greg Xu <gouwa5700@gmail.com>
2022-06-03 22:21:58 +0800

    fix duplicate route names

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    delete 'sign_out', :to => 'devise/sessions#destroy'
   end
   resources :todos do
     member do
