f5c0b10cbef26cc85dac1a0f707749655d7cfd7e
Greg Xu <gouwa5700@gmail.com>
2022-06-07 08:36:42 +0800

    dashboard as root path

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: redirect('https://goooooouwa.github.io/goplan-web/')
+  root to: "admin/dashboard#index"
 end
