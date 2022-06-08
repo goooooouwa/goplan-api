938ebca5177c5ac48b55eadb2ac3536a9c63b344
Greg Xu <gouwa5700@gmail.com>
2022-06-08 09:21:33 +0800

    allow name and image url when sign up or edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image_url])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image_url])
+  end
 end
