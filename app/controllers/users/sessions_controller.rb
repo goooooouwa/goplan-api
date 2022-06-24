e2363de39ff3043acae5e1f61597d6bed67da26b
Shunfa Xu <gouwa5700@gmail.com>
2022-06-24 21:23:47 +0800

    wip

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+  before_action -> { doorkeeper_authorize! :write }, only: %i[destroy_with_token]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  def destroy_with_token
+    if current_user
+      sign_out current_user
+    end
+  end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,14 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   use_doorkeeper
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+  devise_scope :users, do
+    delete "users/sign_out_with_token", to: "users/sessions#destroy_with_token", as: :destroy_user_session_with_token
+  end
+
   resources :todos do
     member do
       put :dependencies, to: 'todos#update_dependencies'
@@ -20,4 +27,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to: "home#index"
+  # root to: redirect(ENV['GOPLAN_WEB_BASE_URL'])
 end
