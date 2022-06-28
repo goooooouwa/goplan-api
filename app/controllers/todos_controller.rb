d13e67516dd2da46acdc93841701c96cb980f3f8
Shunfa Xu <gouwa5700@gmail.com>
2022-06-29 00:24:11 +0800

    build_todo_dependencies_attributes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -28,8 +28,9 @@ class TodosController < ApiController
 
   # PATCH/PUT /todos/1/dependencies
   def update_dependencies
-    @todo.dependencies = Todo.find todo_params[:dependencies_attributes]
-    if @todo.save
+    params[:todo][:todo_dependencies_attributes] = build_todo_dependencies_attributes(@todo, params[:dependencies_attributes])
+
+    if @todo.update(todo_params)
       render 'todos/show'
     else
       render json: @todo.errors, status: :unprocessable_entity
@@ -52,6 +53,29 @@ class TodosController < ApiController
 
   private
 
+  def build_todo_dependencies_attributes(todo, dependencies_attributes)
+    todo_dependencies_attributes = []
+
+    to_be_destroyed = todo.todo_dependencies.where.not(todo_id: dependencies_attributes)
+    to_be_destroyed.each do |todo_dependency|
+      todo_dependencies_attributes << {
+        id: todo_dependency.id,
+        _destory: '1'
+      }
+    end
+
+    new_dependencies = Todo.find dependencies_attributes
+    new_dependencies.each do |dependency|
+      todo_dependency = TodoChild.find_by todo_id: dependency.id, child_id: todo.id
+      todo_dependencies_attributes << {
+        id: todo_dependency.nil? ? nil : todo_dependency.id,
+        todo_id: dependency.id,
+      }.compact
+    end
+
+    todo_dependencies_attributes
+  end
+
   # Use callbacks to share common setup or constraints between actions.
   def set_todo
     @todo = Todo.find(params[:id])
@@ -62,8 +86,7 @@ class TodosController < ApiController
   def todo_params
     params.require(:todo).permit(:project_id, :name, :description, :status, :time_span, :start_date, :end_date, :repeat,
                                  :repeat_period, :repeat_times, :instance_time_span,
-                                 todo_dependents_attributes: [:child_id],
-                                 todo_dependencies_attributes: [:todo_id],
-                                 dependencies_attributes: [])
+                                 todo_dependents_attributes: [:id, :todo_id, :child_id, :_destroy],
+                                 todo_dependencies_attributes: [:id, :todo_id, :child_id, :_destroy])
   end
 end
