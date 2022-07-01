35b43da340396712beddc98f3575466ef4097688
Shunfa Xu <gouwa5700@gmail.com>
2022-07-02 00:30:36 +0800

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,8 @@ class User < ApplicationRecord
     puts auth[:provider]
     puts auth[:uid]
     where(provider: auth[:provider], uid: auth[:uid]).first_or_create do |user|
+      user.provider = auth[:provider]
+      user.uid = auth[:uid]
       user.email = auth[:info][:email]
       user.password = Devise.friendly_token[0, 20]
       user.name = auth[:info][:name] # assuming the user model has a name
