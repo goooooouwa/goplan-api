010417c2a9c7037d892c7d2d06bf914f127a32e0
Shunfa Xu <gouwa5700@gmail.com>
2022-07-02 00:00:14 +0800

    fix: hash access

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,12 +19,12 @@ class User < ApplicationRecord
   has_many :todos, through: :projects
 
   def self.from_omniauth(auth)
-    console.log(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
+    puts auth
+    where(provider: auth[:provider], uid: auth[:uid]).first_or_create do |user|
+      user.email = auth[:info][:email]
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name # assuming the user model has a name
-      user.image_url = auth.info.image # assuming the user model has an image
+      user.name = auth[:info][:name] # assuming the user model has a name
+      user.image_url = auth[:info][:image] # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
