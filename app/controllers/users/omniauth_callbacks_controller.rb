932303297bb723fb6bf37132ce183b3ef893777b
Shunfa Xu <gouwa5700@gmail.com>
2022-07-01 23:43:37 +0800

    access a hash either string or symbol keys

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,45 +3,32 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: [:github, :google_oauth2]
 
   def github
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
-    else
-      session['devise.oauth.data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url
-    end
+    callback
   end
 
   def google_oauth2
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
-    else
-      session['devise.oauth.data'] = request.env['omniauth.auth'].except("extra") # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url
-    end
+    callback
   end
 
   def wechat
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: 'WeChat') if is_navigational_format?
-    else
-      session['devise.oauth.data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url
-    end
+    callback
   end
 
   def failure
     redirect_to root_path
+  end
+
+  private
+
+  def callback
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+    else
+      session['devise.oauth.data'] = request.env['omniauth.auth'].with_indifferent_access.except(:extra) # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url
+    end
   end
 end
