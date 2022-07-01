be5f7c57b923b8edf0ccdb23bafc0cda01fc9b74
Shunfa Xu <gouwa5700@gmail.com>
2022-07-01 13:36:41 +0800

    add omni sign in links

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[github]
+         :omniauthable, omniauth_providers: [:github, :wechat, :google_oauth2]
 
   has_many :access_grants,
            class_name: 'Doorkeeper::AccessGrant',
