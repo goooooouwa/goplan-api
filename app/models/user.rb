e60e2b22df1ecd93529e758720800d035ef7d0aa
Shunfa Xu <gouwa5700@gmail.com>
2022-07-02 00:15:32 +0800

    get info.email

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
   def self.new_with_session(params, session)
     super.tap do |user|
       if (data = session['devise.oauth.data'] && session['devise.oauth.data']['info']) && user.email.blank?
-        user.email = data['email']
+        user.email = data['info']['email']
       end
     end
   end
