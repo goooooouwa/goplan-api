5f1a216e8d9537b8d804e978463cc32d5bcecedb
Shunfa Xu <gouwa5700@gmail.com>
2022-07-04 14:07:27 +0800

    update defaults

--- db/schema.rb
+++ db/schema.rb
@@ -114,13 +114,13 @@ ActiveRecord::Schema.define(version: 2022_07_03_135633) do
     t.bigint "project_id", null: false
     t.string "name", null: false
     t.text "description", default: "", null: false
-    t.float "time_span", default: 0.0, null: false
+    t.float "time_span", default: 1.0, null: false
     t.datetime "start_date", null: false
     t.datetime "end_date", null: false
     t.boolean "repeat", default: false, null: false
-    t.string "repeat_period", default: "", null: false
-    t.integer "repeat_times", default: 0, null: false
-    t.integer "instance_time_span", default: 0, null: false
+    t.string "repeat_period", default: "week", null: false
+    t.integer "repeat_times", default: 1, null: false
+    t.integer "instance_time_span", default: 1, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "status", default: false
